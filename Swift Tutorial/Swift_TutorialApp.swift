8cea76dd3e20853f7e1cabdf962a0eb2855530f6
Javier Martinez <javierrrr.15@gmail.com>
2024-05-27 21:35:13 -0600

    Initial Commit

---
+++ Swift Tutorial/Swift_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_TutorialApp.swift
+//  Swift Tutorial
+//
+//  Created by Javier Martinez on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
